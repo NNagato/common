975b2fad56c1558efbd23c4781356ea418872528
NNagato <darkdragon236@gmail.com>
2021-06-25 11:59:50 +0700

    add more pointer

--- common_pointer.go
+++ common_pointer.go
@@ -1,5 +1,7 @@
 package common
 
+import "time"
+
 // PoiterString ...
 func PoiterString(s string) *string {
 	return &s
@@ -12,5 +14,19 @@ func PoiterUint64(u uint64) *uint64 {
 
 // PoiterInt64 ...
 func PoiterInt64(i int64) *int64 {
+	return &i
+}
+
+// PoiterFloat64 ...
+func PoiterFloat64(f float64) *float64 {
+	return &f
+}
+
+func PointerTime(t time.Time) *time.Time {
+	return &t
+}
+
+// PoiterInt ...
+func PoiterInt(i int) *int {
 	return &i
 }
